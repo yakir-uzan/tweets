c7dc30d1e01faf831dcd4a77ce6d33487ab33827
Yakir Uzan <yakiruzangreen@gmail.com>
2025-08-04 17:19:17 +0300

    added more 2 func analyz

--- src/dataAnalyzer.py
+++ src/dataAnalyzer.py
@@ -24,9 +24,26 @@ class DataAnalyzer:
             "total": round(float(total),2)
         }
 
+    def longest_3_tweets(self, n = 3):
+        df = self.df.copy()
+        df['Length'] = df['Text'].apply(len)
+        result = {}
+        for category in 1, 0:
+            tweets = df[df['Biased'] == category].sort_values(by ='Length', ascending=False)['Text'].head(n).tolist()
+            result["antisemitic" if category == 1 else "non_antisemitic"] = tweets
+        return result
+
+    def common_words(self, n=10):
+        df = self.df.copy()
+        df['Words'] = df['Text'].str.split()
+        words_series = df.explode('Words')['Words']
+        top_words = words_series.value_counts().head(n).index.tolist()
+        return {f"The 10 most common words in all tweets: {top_words}"}
 
 
 
 da = DataAnalyzer("C:/Users/yakir/PycharmProjects/tweets/data/tweets_dataset.csv")
 print(da.total_tweets())
 print(da.average_length())
+print(da.longest_3_tweets())
+print(da.common_words())
